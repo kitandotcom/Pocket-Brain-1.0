f73d67e93d321a693ec748d74fb27aa54df60119
kitandotcom <aderounmuisrael6@gmail.com>
2026-04-07 19:04:18 +0100

    Update parse-bank-statement.js

--- api/parse-bank-statement.js
+++ api/parse-bank-statement.js
@@ -1,4 +1,3 @@
-// /api/parse-bank-statement.js - Supports ALL Nigerian Banks
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
